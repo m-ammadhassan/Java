2e6c48230686de8e1b745fa2a5ed5b0e56d19df2
Muhammad Ammad Hassan <m.ammadhassan@yahoo.com>
2024-07-19 00:31:46 +0100

    Hello World by Muhammad Ammad Hassan

--- FirstSteps/src/HelloWorld.java
+++ FirstSteps/src/HelloWorld.java
@@ -2,5 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Hello World
         System.out.println("Hello World");
+
+        // New Hello
+        System.out.println("Hello World by Muhammad Ammad Hassan using Java");
     }
 }
